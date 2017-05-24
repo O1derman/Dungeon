29ec31bfa40070a624345d6474704de81c7c6bf9
Filip <abc55@outlook.cz>
2017-05-24 13:39:56 +0200

    RandomValue class

---
+++ core/src/main/java/com/example/olderman/dungeon/RandomValue.java
@@ -0,0 +1,47 @@
+package com.example.olderman.dungeon;
+
+public class RandomValue {
+	private final int constantFactor;
+	private final int randomFactor;
+	private final int floorFactor;
+	private final int levelFactor;
+
+	public RandomValue(int constantFactor, int randomFactor, int floorFactor, int levelFactor) {
+		this.constantFactor = constantFactor;
+		this.randomFactor = randomFactor;
+		this.floorFactor = floorFactor;
+		this.levelFactor = levelFactor;
+	}
+
+	public RandomValue(int constantFactor, int randomFactor) {
+		this(constantFactor, randomFactor, 0, 0);
+	}
+
+	public RandomValue(int constantFactor, int randomFactor, int floorFactor) {
+		this(constantFactor, randomFactor, floorFactor, 0);
+	}
+
+	public int getConstantFactor() {
+		return constantFactor;
+	}
+
+	public int getRandomFactor() {
+		return randomFactor;
+	}
+
+	public int getFloorFactor() {
+		return floorFactor;
+	}
+
+	public int getLevelFactor() {
+		return levelFactor;
+	}
+
+	public int nextValue(Dungeon dungeon) {
+		return constantFactor
+				+ (randomFactor == 0 ? 0
+						: dungeon.rand.nextInt(randomFactor / 2) + dungeon.rand.nextInt((randomFactor + 1) / 2))
+				+ floorFactor * dungeon.getForAll().floor + levelFactor * dungeon.getForAll().level;
+	}
+
+}
