d77dccc84e760dd1c2829444fa368ade407a82e6
DeeJay <81mjanousek@gmail.com>
2018-01-03 09:38:56 +0100

    Moved map characters to one place to make it easier to change look of the map.

---
+++ core/src/main/java/olderman/dungeon/map/MapConstants.java
@@ -0,0 +1,16 @@
+package olderman.dungeon.map;
+
+
+public class MapConstants {
+
+    public static final char wallChar = (char) 0x25A0;
+
+    public static final char playerChar =  (char) 0x263B;
+
+    public static final char bossChar = 'A';
+
+    public static final char clearRoomChar = 'c';
+
+    public static final char fullRoomChar = 'o';
+
+}
